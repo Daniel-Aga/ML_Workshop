657179747032c2d30e3ad09eab0a25b73510ddac
Itay Sherman <itay2001@gmail.com>
2021-09-06 15:41:20 +0300

    Add files via upload

--- background subtraction/blob.py
+++ background subtraction/blob.py
@@ -7,18 +7,16 @@ THRESHOLD = 135
 UNLABELED_FOLDER = 'unlabeled_hanadiv'
 OUTS_FOLDER = 'outs_hanadiv'
 BOX_FOLDER = 'box_hanadiv'
-# filename = 'WSCT0250.JPG'
+# filename = 'outs_hanadiv/WSCT0250.JPG'
 
 def get_bounding_boxes(filename):
-    outsname = f'{OUTS_FOLDER}/{filename}'
-    realname = f'{UNLABELED_FOLDER}/{filename}'
-    boxname = f'{BOX_FOLDER}/{filename}'
-    im = cv2.imread(outsname, cv2.IMREAD_GRAYSCALE)
-    width, height = im.shape[:2]
+    """
+    Returns a list of (min_x, min_y, max_x, max_y) each representing a bounding box
+    of an insect
+    """
+    im = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
 
     ret, threshed = cv2.threshold(im, THRESHOLD, 255, cv2.THRESH_BINARY_INV)
-    # cv2.imshow('threshed', threshed)
-    # cv2.waitKey(0)
     contours, hierarchy = cv2.findContours(threshed, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
     threshed = cv2.cvtColor(threshed, cv2.COLOR_GRAY2RGB)
     boxes = []
@@ -36,15 +34,17 @@ def get_bounding_boxes(filename):
         max_x = int(cx + RATIO * (max_x - cx))
         max_y = int(cy + RATIO * (max_y - cy))
         boxes.append((min_x, min_y, max_x, max_y))
-    # return boxes
+    return boxes
 
-    
-    # cv2.imshow('after boxes', threshed)
-    # cv2.waitKey(0)
+def save_with_boxes(filename):
+    outsname = f'{OUTS_FOLDER}/{filename}'
+    realname = f'{UNLABELED_FOLDER}/{filename}'
+    boxname = f'{BOX_FOLDER}/{filename}'
+    boxes = get_bounding_boxes(outsname)
+
     real_im = cv2.imread(realname)
     for box in boxes:
         min_x, min_y, max_x, max_y = box
-        # cv2.rectangle(threshed, (min_x, min_y), (max_x, max_y), (255, 0, 0), 1)
         cv2.rectangle(
             real_im,
             (min_x / SIZE_FACTOR, min_y / SIZE_FACTOR),
@@ -56,4 +56,4 @@ def get_bounding_boxes(filename):
 # get_bounding_boxes(filename)
 
 for i in range(1, 461 + 1):
-    get_bounding_boxes(f'WSCT{str(i).zfill(4)}.JPG')
+    save_with_boxes(f'WSCT{str(i).zfill(4)}.JPG')
